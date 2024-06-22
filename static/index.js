80734dcd93243719208894761fa538ecf0ae7830
TanakAiko <cherifmbaye02@gmail.com>
2024-06-22 01:49:17 +0000

    Can logout with the handling of cookie

--- static/index.js
+++ static/index.js
@@ -1,3 +1,5 @@
+var ws
+
 function toggleForms() {
     var loginForm = document.getElementById('loginForm');
     var registerForm = document.getElementById('registerForm');
@@ -21,7 +23,6 @@ function toggleForms() {
         }, 500);
     }
 }
-
 
 var registerFormID = document.getElementById("registerFormID")
 registerFormID.addEventListener("submit", async (event) => {
@@ -53,8 +54,6 @@ registerFormID.addEventListener("submit", async (event) => {
 })
 
 
-//********************************************************************************************************************** */
-
 var loginFormID = document.getElementById("loginFormID")
 loginFormID.addEventListener("submit", async (event) => {
     event.preventDefault()
@@ -76,25 +75,61 @@ loginFormID.addEventListener("submit", async (event) => {
         const result = await response.json()
         console.log(result);
 
-         // Allow time for the cookie to be set
-         setTimeout(() => {
-            console.log("document.cookie: ", document.cookie);
-        }, 1000);
-
-        let ws = new WebSocket("ws://localhost:8080/ws");
-
-        ws.onerror = function (error) {
-            console.error("WebSocket Error: ", error);
-        };
+        ws = new WebSocket("ws://localhost:8080/ws");
 
         ws.onopen = function () {
             console.log("Connection is open...");
+        };
+
+        ws.onmessage = onMessage
+
+        ws.onerror = onError
+
+        ws.onclose = function () {
+            console.log("Connection closed !!!");
         };
 
     } catch (error) {
         console.error(error);
     }
 })
+
+//********************************************************************************************************************** */
+
+var logoutButton = document.getElementById("logoutButton")
+logoutButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    const sessionId = getCookieValue("sessionID")
+
+    if (ws && sessionId) {
+        ws.send(JSON.stringify({ action: "logout", data: sessionId }));
+    } else {
+        console.error("WebSocket is not initialized or sessionID is null.");
+    }
+})
+
+//********************************************************************************************************************** */
+
+function onMessage(event) {
+    const msg = JSON.parse(event.data);
+    console.log('Message from server:', msg);
+
+    switch (msg.action) {
+        case 'reply':
+            console.log('Server replied (', msg.action, '): ', msg.data);
+            break;
+        case 'logout':
+            console.log('Server replied (', msg.action, '): ', msg.data);
+            deleteCookie("sessionID")
+            break;
+        default:
+            console.log('Unknown action:', msg.action);
+    }
+}
+
+function onError(error) {
+    console.error('WebSocket error:', error);
+}
 
 //********************************************************************************************************************** */
 
@@ -107,4 +142,15 @@ function getDataForm(form) {
 function strToInt(str) {
     num = Number(str)
     return parseInt(num, 10)
+}
+
+function getCookieValue(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
+}
+
+function deleteCookie(name) {
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 }
